ba1fd4a98a3b3f4c1a4fec8433a6a05ddd4028b1
kartemake <a.kuskov@make.st>
2023-01-05 15:43:14 +0700

    edited component 'main'

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Button from '../Button/Button';
 
 function Main() {
-    return(
-        <>Main</>
-    );
+  return (
+    <main className='container'>
+      <div className='container__content'>
+        <p>This is&nbsp;about canned food from the cyberpunk future, when food was unified and different substrates began to&nbsp;be&nbsp;packaged in&nbsp;cans under unnamed numbers. This is&nbsp;about what is&nbsp;eternal, what is&nbsp;not subject to&nbsp;momentary trends, what is&nbsp;forever.</p>
+      </div>
+      <Button />
+    </main>
+  );
 }
 
 export default Main;
