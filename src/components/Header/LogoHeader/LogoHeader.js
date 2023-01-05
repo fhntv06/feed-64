58b47c32f4f440244c509478627f17892c762438
kartemake <a.kuskov@make.st>
2023-01-05 15:42:19 +0700

    edited component "logo"

--- src/components/Header/LogoHeader/LogoHeader.js
+++ src/components/Header/LogoHeader/LogoHeader.js
@@ -1,11 +1,10 @@
 import React from "react";
-import './LogoHeader.module.scss';
 
 function LogoHeader() {
   return(
-    <div styleName='header__logo'>
-      <div styleName='logo__title'>FEED 64</div>
-      <div styleName='logo__dectiprion'>Canned Interactive</div>
+    <div className='header__logo'>
+      <div className='logo__title'>FEED 64</div>
+      <div className='logo__dectiprion'>Canned Interactive</div>
     </div>
   );
 }
