ef4248ac3e9bcb7fd086185ec62d913b7ef259c8
kartemake <a.kuskov@make.st>
2023-01-06 17:45:24 +0700

    edited show screens Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,43 @@
 import React from "react";
-import Button from '../Button/Button';
+import AboutScreen from '../Screens/AboutScreen';
+import GennadyScreen from '../Screens/GennadyScreen';
+import ThrowormScreen from '../Screens/ThrowormScreen';
+import BlackCrowScreen from '../Screens/BlackCrowScreen';
+import ContactsScreen from '../Screens/ContactsScreen';
+//  через редакс сделать получение объекта с данными страниц
 
 function Main() {
   return (
-    <main className='container'>
-      <div className='container__content'>
-        <p>This is&nbsp;about canned food from the cyberpunk future, when food was unified and different substrates began to&nbsp;be&nbsp;packaged in&nbsp;cans under unnamed numbers. This is&nbsp;about what is&nbsp;eternal, what is&nbsp;not subject to&nbsp;momentary trends, what is&nbsp;forever.</p>
+    <main className='main grid-container'>
+      <div id="about" className='main__content grid-container bg bg-about'>
+        <div className="container grid-container offset--out-top">
+          <AboutScreen />
+        </div>
       </div>
-      <Button />
+
+      <div id="gennady" className='main__content grid-container bg bg-gennady'>
+        <div className="container grid-container">
+          <GennadyScreen />
+        </div>
+      </div>
+
+      <div id="throworm" className='main__content grid-container bg bg-throworm'>
+        <div className="container grid-container">
+          <ThrowormScreen />
+        </div>
+      </div>
+
+      <div id="blackCrow" className='main__content grid-container bg'>
+        <div className="container grid-container">
+          <BlackCrowScreen />
+        </div>
+      </div>
+
+      <div id="contacts" className='main__content grid-container bg bg-contact'>
+        <div className="container grid-container offset--out-top">
+          <ContactsScreen />
+        </div>
+      </div>
     </main>
   );
 }
